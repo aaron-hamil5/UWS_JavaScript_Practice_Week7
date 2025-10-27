2f08f5b88feeb90f39fb7b4e0170fd40a2eccf62
Aaron Hamilton <aaronleon28@gmail.com>
2025-10-27 12:35:21 +0000

    Now we add it to a map and load data from a map

--- javascript/grades.js
+++ javascript/grades.js
@@ -1,10 +1,19 @@
+var studentData = new Map()
+
+
 function addData(studentName, studentGrade){
+    studentData.set(studentName, studentGrade);
+    printData();
+}
+
+function printData(){
     const studentGradeTable = document.getElementById("student_grade_table");
     const newRow = studentGradeTable.insertRow();
 
+    studentData.forEach((studentGrade, studentName) =>{
     newRow.innerHTML = `
         <td>${studentName}</td>
         <td>${studentGrade}</td>
     `;
-
+    }, this);
 }
